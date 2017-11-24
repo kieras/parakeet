5363c46d022e5c9bfb6ed3351f7dbf0ab5da67fd
Kieras <akieras@ciandt.com>
2017-11-24 15:01:02 -0200

    Adding a common step.

---
+++ parakeet/common_steps.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from lettuce import step, world
+from auth import LoginPage
+
+
+@step(u'.* the logged user is "([^"]*)"')
+def login(step, user_name):
+    # If it is already logged do not attempt to login
+    if world.cfg['system_page_title'] not in world.browser.driver.title:
+        email = world.cfg['users'][user_name]['email']
+        password = world.cfg['users'][user_name]['password']
+        LoginPage(world.browser, world.cfg['system_page_title'])\
+            .fill_email(email)\
+            .click_next()\
+            .fill_password(password)\
+            .login()\
+            .redirect_to_home()
