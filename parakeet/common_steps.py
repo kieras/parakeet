2490a620cf2f46f87370214001871c5d5844f556
Kieras <akieras@ciandt.com>
2018-01-02 19:38:15 -0200

    Fix title check.

--- parakeet/common_steps.py
+++ parakeet/common_steps.py
@@ -6,7 +6,7 @@ from auth import LoginPage
 @step(u'.* the logged user is "([^"]*)"')
 def login(step, user_name):
     # If it is already logged do not attempt to login
-    if world.cfg['system_page_title'] not in world.browser.driver.title:
+    if world.cfg['system_page_title'] not in world.browser.selenium.title:
         email = world.cfg['users'][user_name]['email']
         password = world.cfg['users'][user_name]['password']
         LoginPage(world.browser, world.cfg['system_page_title'])\
