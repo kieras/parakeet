e1fcec6e3e769b4c2b8c78d6af3ed03a3719353e
Kieras <akieras@ciandt.com>
2018-01-23 11:50:16 -0200

    Isolate users from configs, preparing for externalize users data.

--- parakeet/common_steps.py
+++ parakeet/common_steps.py
@@ -7,8 +7,8 @@ from auth import LoginPage
 def login(step, user_name):
     # If it is already logged do not attempt to login
     if world.cfg['system_page_title'] not in world.browser.selenium.title:
-        email = world.cfg['users'][user_name]['email']
-        password = world.cfg['users'][user_name]['password']
+        email = world.users[user_name]['email']
+        password = world.users[user_name]['password']
         LoginPage(world.browser, world.cfg['system_page_title'])\
             .fill_email(email)\
             .click_next()\
--- parakeet/utils.py
+++ parakeet/utils.py
@@ -10,7 +10,7 @@ def load_config(yaml_file):
     :type yaml_file: str
     :return: a dict.
     """
-    print('Loading configs from {0} file.'.format(yaml_file))
-    with open(yaml_file, 'r') as config_file:
-        config_dict = yaml.load(config_file)
-    return config_dict
+    print('Loading file: {}.'.format(yaml_file))
+    with open(yaml_file, 'r') as f:
+        yaml_content = yaml.load(f)
+    return yaml_content
